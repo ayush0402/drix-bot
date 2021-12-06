f1c507faefeb81bee66031879d45070105e26969
ayush0402 <lit2020024@iiitl.ac.in>
2021-12-06 19:24:09 +0530

    removed dotenv java usage

--- src/main/java/Config.java
+++
@@ -1,9 +0,0 @@
-import io.github.cdimascio.dotenv.Dotenv;
-
-public class Config {
-    private static final Dotenv dotenv = Dotenv.load();
-
-    public static String get(String x){
-        return dotenv.get(x);
-    }
-}
